ab4c12f05d57b7b2d49053ea8c8c155a9cdacb9a
Ante Sosa <antesosa95@gmail.com>
2020-10-20 09:30:37 +0200

    Add latest block information

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { DeveloperConsole } from './substrate-lib/components';
 import AccountSelector from './AccountSelector';
 import Balances from './Balances';
 import BlockNumber from './BlockNumber';
+import BlockInformation from './BlockInformation';
 import Events from './Events';
 import Interactor from './Interactor';
 import Metadata from './Metadata';
@@ -60,6 +61,9 @@ function Main () {
             <Metadata />
             <BlockNumber />
             <BlockNumber finalized />
+          </Grid.Row>
+          <Grid.Row stretched>
+            <BlockInformation />
           </Grid.Row>
           <Grid.Row stretched>
             <Balances />
